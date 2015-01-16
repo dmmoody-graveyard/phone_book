25db8d9c9ae8ab51644a012a814f51f51f623aec
Duane M Moody <dmmoody@gmail.com>
2015-01-16 09:42:35 -0800

    add Contact#phone_number spec

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,10 +3,17 @@ require('contact')
 
 describe(Contact) do
   
-  describe('name') do
+  describe('#name') do
     it('will take name and phone_number as hash and return name') do
       new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
       expect(new_contact.name()).to(eq("Duane Moody"))
+    end
+  end
+
+  describe('#phone_number') do
+    it('will take name and phone_number as hash and return phone_number') do
+      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      expect(new_contact.phone_number()).to(eq("971-678-9823"))
     end
   end
 end
