c05ebaf295fce7470c7fa9a6d7d91b18c2820ebf
Duane M Moody <dmmoody@gmail.com>
2015-01-16 13:20:13 -0800

    refactor contacts with new init variables

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -9,15 +9,15 @@ describe(Contact) do
   
   describe('#name') do
     it('will take name and phone_number as hash and return name') do
-      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact = Contact.new('Duane Moody')
       expect(new_contact.name()).to(eq("Duane Moody"))
     end
   end
 
-  describe('#phone_number') do
+  describe('#phone_numbers') do
     it('will take name and phone_number as hash and return phone_number') do
-      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
-      expect(new_contact.phone_number()).to(eq("971-678-9823"))
+      new_contact = Contact.new('Duane Moody')
+      expect(new_contact.phone_numbers()).to(eq([]))
     end
   end
 
@@ -29,14 +29,14 @@ describe(Contact) do
 
   describe('#id') do
     it('returns the id of the contact') do
-      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact = Contact.new('Duane Moody')
       expect(new_contact.id()).to(eq(1))
     end
   end
 
   describe('#save') do
     it("will save contact to contact list") do
-      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact = Contact.new('Duane Moody')
       new_contact.save()
       expect(Contact.all()).to(eq([new_contact]))
     end
@@ -44,7 +44,7 @@ describe(Contact) do
 
   describe('.clear') do
     it('will clear contact list of all elements') do
-      new_contact = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact = Contact.new('Duane Moody')
       new_contact.save()
       expect(Contact.clear()).to(eq([]))
     end
@@ -52,9 +52,9 @@ describe(Contact) do
 
   describe('.find_id') do
     it('will find a contact by id number') do
-      new_contact1 = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact1 = Contact.new('Duane Moody')
       new_contact1.save()
-      new_contact2 = Contact.new({:name => 'Steve Two Two', :phone_number => '971-678-6077'})
+      new_contact2 = Contact.new('Duane Moody')
       new_contact2.save()
       expect(Contact.find_id(new_contact1.id())).to(eq(new_contact1))
     end
@@ -62,9 +62,9 @@ describe(Contact) do
 
   describe('.find_contact') do
     it('will find a contact by name') do
-      new_contact1 = Contact.new({:name => 'Duane Moody', :phone_number => '971-678-9823'})
+      new_contact1 = Contact.new('Duane Moody')
       new_contact1.save()
-      new_contact2 = Contact.new({:name => 'Steve Two Two', :phone_number => '971-678-6077'})
+      new_contact2 = Contact.new('Steve Two Two')
       new_contact2.save()
       expect(Contact.find_contact(new_contact1.name())).to(eq(new_contact1))
     end
