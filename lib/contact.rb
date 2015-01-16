d68e44e0a1fb8acd048e611c8560d334b24ef258
Duane M Moody <dmmoody@gmail.com>
2015-01-16 10:01:34 -0800

    add Contact#all method

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,9 +1,18 @@
 class Contact
 
+  @@contact_list = []
   attr_reader(:name, :phone_number)
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @phone_number = attributes.fetch(:phone_number)
+  end
+
+  define_singleton_method(:all) do
+    @@contact_list
+  end
+
+  define_singleton_method(:add_contact) do
+    @@contact_list.push(self)
   end
 end
