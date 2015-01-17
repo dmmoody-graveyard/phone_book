f82ba0742ac341624ac119d58cdfc443378346e2
Duane M Moody <dmmoody@gmail.com>
2015-01-16 16:03:16 -0800

    add index.html

---
+++ app.rb
@@ -0,0 +1,34 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('./lib/**/*.rb')
+require('./lib/contact')
+require('./lib/phone')
+
+get('/') do
+  @contacts = Contact.all()
+  erb(:index)
+end
+
+get('/add_contact') do
+  erb(:add_contact_form)
+end
+
+get('/:content_id') do
+  contact_id = params.fetch('content_id'.to_i())
+  @contact = Content.find_id(contact_id)
+  erb(:contact_details)
+end
+
+post('/contact_success') do
+  new_contact = Contact.new(params.fetch('name'))
+  new_contact.save()
+  redirect('/')
+end
+
+post('/phone_success') do
+  phone_type = params.fetch('phone_type')
+  number = params.fetch('number')
+  new_phone = Phone.new(:type => phone_type, :number => number)
+  new_phone.save()
+  redirect('/')
+end
